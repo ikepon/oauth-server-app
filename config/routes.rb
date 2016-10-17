cf0fd3ac04fd08c167aa0a520982e7bd1303c245
ikepon <tatsuyanoheya@gmail.com>
2016-10-17 16:46:31 +0900

    ログイン用のrouteを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   # doorkeeper で root_path が必要なので仮で設定
   root :to => 'users#index'
-
+  resources :user_sessions
   resources :users
   use_doorkeeper
 
   get '/me' => 'application#me'
+  get 'login' => 'user_sessions#new', :as => :login
+  post 'logout' => 'user_sessions#destroy', :as => :logout
 end
