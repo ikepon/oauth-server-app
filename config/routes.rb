9e471d373b9f6d95f12f3bee4b219fdf33de5c6b
ikepon <tatsuyanoheya@gmail.com>
2016-10-17 15:43:54 +0900

    rails g scaffold user email:string crypted_password:string salt:string --migration false

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # doorkeeper で root_path が必要なので仮で設定
-  root :to => 'users#root'
+  root :to => 'users#index'
 
+  resources :users
   use_doorkeeper
 
   get '/me' => 'application#me'
